cd2280d03710c1d78ece332864c5f32e6d14758e
Vivek18 <viveksaha096@gmail.com>
2026-03-29 07:30:49 +0530

    Inner Joins added!

---
+++ Joins/innerJoins.sql
@@ -0,0 +1,23 @@
+Create table tblJoinsDemo(
+id int primary key identity,
+name varchar(50)
+)
+Insert Into tblJoinsDemo(name) values('Vivek')
+Insert Into tblJoinsDemo(name) values('Rahul')
+Create table tblJoins(
+studentId int foreign key(studentId) references tblJoinsDemo(id),
+marks int 
+)
+Delete from tblJoins where studentId=1
+Delete from tblJoins where marks=45
+Insert Into tblJoins(studentId, marks)values(1, 99)
+Insert Into tblJoins(studentId, marks)values(null, 45)
+
+Select * from tblJoinsDemo
+Select * from tblJoins
+
+Select * from 
+tblJoins j
+inner join 
+tblJoinsDemo d
+on j.studentId=d.id
