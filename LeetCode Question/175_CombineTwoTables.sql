f5401a9347c2f7fbfc43ad3ba6d824b22f693dc4
Vivek18 <viveksaha096@gmail.com>
2026-04-11 08:13:33 +0530

    175 LeetCode Question Done!

---
+++ LeetCode Question/175_CombineTwoTables.sql
@@ -0,0 +1,17 @@
+/* Write your T-SQL query statement below */
+
+/*
+Syntax
+Select column_name(s)
+From table1
+LEFT JOIN table2
+ON table1.column_name = table2.column_name;
+
+Left Join returns all records from the left table (table1), and the matched records from the right table (table2). The result is NULL from the right side, if there is no match.
+ */
+
+SELECT firstName, lastName, city, state 
+from Person p
+LEFT JOIN 
+Address a 
+ON p.personId = a.personId
